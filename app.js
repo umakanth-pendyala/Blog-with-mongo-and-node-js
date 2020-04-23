2b87501b417f9c857c17f99cc5bc60c6a722e9f1
umakanth pendyala <umakanthpendyala@gmail.com>
2020-04-23 08:58:35 +0530

    complete final

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-var url = "mongodb+srv://umakanthpendyala:posts123@newtestserver-abxpk.mongodb.net/postDb";
 
 mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if (err) {

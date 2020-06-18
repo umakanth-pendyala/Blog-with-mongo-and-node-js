77289db18e82fd3a890c4ae9125d066d96453c8a
umakanth pendyala <umakanthpendyala@gmail.com>
2020-06-18 17:37:09 +0530

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const stringModifier = require("lodash");
+const https = require("https");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -165,7 +166,19 @@ app.post("/composed", (req, res) => {
 })
 
 
-app.listen(process.env.PORT, () => {
+app.get("/covid_api/:option/yesterday", (req, res) => {
+  console.log('went here');
+  if (req.params.option == "India") {
+    https.get('https://api.covid19api.com/live/country/india/status/confirmed', (data, err) => {
+      console.log(data);
+      res.json(data);
+    })
+  }
+    
+})
+
+
+app.listen(process.env.PORT || 3000, () => {
   console.log("port running perfectly");
 })
 
